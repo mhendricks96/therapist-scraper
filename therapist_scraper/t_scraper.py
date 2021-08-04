0905eedfd1def050b1219a717aaffaa594ddad7d
mhendricks96 <mhendricks96@yahoo.com>
2021-08-03 19:08:49 -0700

    getting pd filled out

--- therapist_scraper/t_scraper.py
+++ therapist_scraper/t_scraper.py
@@ -3,7 +3,8 @@ import urllib.request
 import time
 from bs4 import BeautifulSoup
 
-URL = "https://www.psychologytoday.com/us/therapists/or/portland/841246?sid=610343265d839&ref=10"
+
+URL =  "https://www.psychologytoday.com/us/therapists/or/portland/170401?sid=610587f4f295d&ref=4"
 
 headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36"}
 
@@ -20,11 +21,70 @@ title = soup.find("h2").get_text()
 specialties = soup.find("ul", class_="attribute-list specialties-list").get_text()
 
 insurance = soup.find("ul", class_= "attribute-list copy-small").get_text()
-#price = soup.find("div", class_="finances-office").get_text()
+price = soup.find("div", class_="finances-office").get_text()
 
 
 print(name.strip())
 print(zip_code)
 print(specialties)
 print(title.strip())
-print(insurance)
+#print(insurance)
+print(price)
+
+# next_url = soup.find("a", class_="profile-next")["href"]
+
+
+# print(next_url)
+
+# next_page = requests.get(next_url, headers=headers)
+
+# next_soup = BeautifulSoup(next_page.content, "html.parser")
+
+# next_zip_code = next_soup.find(itemprop="postalcode").get_text()
+
+# next_name = next_soup.find(itemprop="name").get_text()
+
+# next_title = next_soup.find("h2").get_text()
+
+# next_specialties = next_soup.find("ul", class_="attribute-list specialties-list").get_text()
+
+# next_insurance = next_soup.find("ul", class_= "attribute-list copy-small").get_text()
+
+# next_price = next_soup.find("div", class_="finances-office").get_text()
+
+
+# print(next_name.strip())
+# print(next_zip_code)
+# print(next_specialties)
+# print(next_title.strip())
+# #print(insurance)
+# print(next_price)
+
+next_url = "https://www.psychologytoday.com/us/therapists/or/portland/170401?sid=610587f4f295d&ref=4"
+for person in range(7):
+
+  next_page = requests.get(next_url, headers=headers)
+
+  next_soup = BeautifulSoup(next_page.content, "html.parser")
+
+  next_zip_code = next_soup.find(itemprop="postalcode").get_text()
+
+  next_name = next_soup.find(itemprop="name").get_text()
+
+  next_title = next_soup.find("h2").get_text()
+
+  next_specialties = next_soup.find("ul", class_="attribute-list specialties-list").get_text()
+
+  next_insurance = next_soup.find("ul", class_= "attribute-list copy-small").get_text()
+
+  next_price = next_soup.find("div", class_="finances-office").get_text()
+
+
+  print(next_name.strip())
+  # print(next_zip_code)
+  # print(next_specialties)
+  # print(next_title.strip())
+  # #print(insurance)
+  # print(next_price)
+
+  next_url = next_soup.find("a", class_="profile-next")["href"]
