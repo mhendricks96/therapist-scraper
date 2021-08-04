e3c74ea3b24940c5384a22804f1e96043a5dd4b2
mhendricks96 <mhendricks96@yahoo.com>
2021-08-04 00:06:37 -0700

    saving before first real attempt

--- therapist_scraper/t_scraper.py
+++ therapist_scraper/t_scraper.py
@@ -2,89 +2,54 @@ import requests
 import urllib.request
 import time
 from bs4 import BeautifulSoup
-
-
-URL =  "https://www.psychologytoday.com/us/therapists/or/portland/170401?sid=610587f4f295d&ref=4"
+import pandas as pd
+import regex
 
 headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36"}
 
-page = requests.get(URL, headers=headers)
+records = []
+next_url = "https://www.psychologytoday.com/us/therapists/or/portland/108093?sid=610a32c3d201b&ref=1"
+for person in range(10):
 
-soup = BeautifulSoup(page.content, "html.parser")
+    next_page = requests.get(next_url, headers=headers)
+    next_soup = BeautifulSoup(next_page.content, "html.parser")
+    next_zip_code = next_soup.find(itemprop="postalcode").get_text()
+    next_name = next_soup.find(itemprop="name").get_text()
+    next_title = next_soup.find("h2").get_text()
+    try:
+        next_specialties = next_soup.find("ul", class_="attribute-list specialties-list").get_text()
+    except AttributeError:
+        next_specialties = "N/A"
+    # next_insurance = next_soup.find("ul", class_= "attribute-list copy-small").get_text()
+    try:
+        next_price = next_soup.find("div", class_="finances-office").get_text().split('\n')
+        while '' in next_price:
+            next_price.remove('')
+        while '            ' in next_price:
+            next_price.remove('            ')
+        while '                                                    ' in next_price:
+            next_price.remove('                                                    ')
+        price = next_price[0]
+    except AttributeError:
+        price = "N/A"
+    
+    
+    records.append((next_name.strip(), next_zip_code, next_title, next_specialties, price))
 
-zip_code = soup.find(itemprop="postalcode").get_text()
+    
+    time.sleep(1)
+    next_url = next_soup.find("a", class_="profile-next")["href"]
 
-name = soup.find(itemprop="name").get_text()
+df = pd.DataFrame(records, columns=["name","zip","title","specialties","price"])
 
-title = soup.find("h2").get_text()
+df["specialties"] = df["specialties"].str.replace(' ','')
 
-specialties = soup.find("ul", class_="attribute-list specialties-list").get_text()
+df[["title","specialties","price"]] = df[["title","specialties","price"]].replace('\n',' ', regex=True)
 
-insurance = soup.find("ul", class_= "attribute-list copy-small").get_text()
-price = soup.find("div", class_="finances-office").get_text()
+df["price"] = df["price"].str.replace('Cost per Session: ','')
 
+df["price"] = df["price"].str.replace(' ','')
 
-print(name.strip())
-print(zip_code)
-print(specialties)
-print(title.strip())
-#print(insurance)
-print(price)
+df["title"] = df["title"].str.replace(' ','')
 
-# next_url = soup.find("a", class_="profile-next")["href"]
-
-
-# print(next_url)
-
-# next_page = requests.get(next_url, headers=headers)
-
-# next_soup = BeautifulSoup(next_page.content, "html.parser")
-
-# next_zip_code = next_soup.find(itemprop="postalcode").get_text()
-
-# next_name = next_soup.find(itemprop="name").get_text()
-
-# next_title = next_soup.find("h2").get_text()
-
-# next_specialties = next_soup.find("ul", class_="attribute-list specialties-list").get_text()
-
-# next_insurance = next_soup.find("ul", class_= "attribute-list copy-small").get_text()
-
-# next_price = next_soup.find("div", class_="finances-office").get_text()
-
-
-# print(next_name.strip())
-# print(next_zip_code)
-# print(next_specialties)
-# print(next_title.strip())
-# #print(insurance)
-# print(next_price)
-
-next_url = "https://www.psychologytoday.com/us/therapists/or/portland/170401?sid=610587f4f295d&ref=4"
-for person in range(7):
-
-  next_page = requests.get(next_url, headers=headers)
-
-  next_soup = BeautifulSoup(next_page.content, "html.parser")
-
-  next_zip_code = next_soup.find(itemprop="postalcode").get_text()
-
-  next_name = next_soup.find(itemprop="name").get_text()
-
-  next_title = next_soup.find("h2").get_text()
-
-  next_specialties = next_soup.find("ul", class_="attribute-list specialties-list").get_text()
-
-  next_insurance = next_soup.find("ul", class_= "attribute-list copy-small").get_text()
-
-  next_price = next_soup.find("div", class_="finances-office").get_text()
-
-
-  print(next_name.strip())
-  # print(next_zip_code)
-  # print(next_specialties)
-  # print(next_title.strip())
-  # #print(insurance)
-  # print(next_price)
-
-  next_url = next_soup.find("a", class_="profile-next")["href"]
+df.to_csv('therapist_data.csv')
